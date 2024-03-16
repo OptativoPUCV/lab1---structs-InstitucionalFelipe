2522e70a39e55d98d9ea9b5799420b71059ffcf8
Your Name <you@example.com>
2024-03-16 16:24:10 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -99,7 +99,7 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
       k++;
     }
   }
-                                    }
+                                    
 
 /*
 Ejercicio 5: Comprobación de Ordenación
