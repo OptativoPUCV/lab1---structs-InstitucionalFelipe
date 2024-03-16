e3fa7735227b6de1179d8b2a08b19673f7ae9cb2
Your Name <you@example.com>
2024-03-16 16:27:29 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -75,31 +75,31 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
   while(i < size1 && j < size2){
     if(arr1[i] < arr2[j])
     {
-      nuevoArr[k] = arr1[i];
+        result[k] = arr1[i];
       i++;
       k++;
     }
     else
     {
-      nuevoArr[k] = arr2[j];
+        result[k] = arr2[j];
       j++;
       k++;
     }
   }
   for( ; i < size1; i++)
     {
-      nuevoArr[k] = arr1[i];
+        result[k] = arr1[i];
       i++;
       k++;
     }
   for( ; j < size2; j++)
     {
-      nuevoArr[k] = arr2[j];
+        result[k] = arr2[j];
       j++;
       k++;
     }
   }
-                                    
+                                    }
 
 /*
 Ejercicio 5: Comprobación de Ordenación
